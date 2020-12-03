71a282db2f1b1a3403cff2b63ba9eaff292d03cb
Cristian <cristian.webber@hotmail.com>
2020-12-03 14:24:37 -0300

    First version

---
+++ src/api-service.js
@@ -0,0 +1,69 @@
+export default class API {
+
+    
+    static loginUser(body) {
+        return fetch(`${process.env.REACT_APP_API_URL}/auth/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify( body )
+        }).then( resp => resp.json())
+        .then( resp => console.log(resp.status))
+    }
+    
+    static registerUser(body) {
+        return fetch(`${process.env.REACT_APP_API_URL}/api/users/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify( body )
+        }).then( resp => resp.json())
+    }
+
+
+    static getMovie(token) {
+        return fetch(`${process.env.REACT_APP_API_URL}/api/movies/`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Token ${token}`
+      }
+    }).then( resp => resp.json())
+    }
+
+    static updateMovie(mov_id, body, token) {
+        return fetch(`${process.env.REACT_APP_API_URL}/api/movies/${mov_id}/`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Token ${token}`
+            },
+            body: JSON.stringify( body )
+        }).then( resp => resp.json())
+    }
+
+
+    static createMovie(body, token) {
+        return fetch(`${process.env.REACT_APP_API_URL}/api/movies/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Token ${token}`
+            },
+            body: JSON.stringify( body )
+        }).then( resp => resp.json())
+    }
+
+    static deleteMovie(mov_id, token) {
+        return fetch(`${process.env.REACT_APP_API_URL}/api/movies/${mov_id}/`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Token ${token}`
+            }
+        })
+    }
+
+}
---
+++ src/components/movie-details.js
@@ -0,0 +1,72 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+import React, { useState } from 'react';
+import { useCookies } from 'react-cookie';
+
+function MovieDetails(props){
+
+    const [highlighted, setHighlighted] = useState (-1);
+    const [token] = useCookies(['mr-token']);
+
+    let mov = props.movie;
+
+    const highlightedRate = high => evt => {
+        setHighlighted(high)
+    }
+
+    const rateClicked = rate => evt => {
+        fetch(`${process.env.REACT_APP_API_URL}/api/movies/${mov.id}/rate_movie/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Token ${token['mr-token']}`
+            },
+            body: JSON.stringify( {stars: rate + 1} )
+        })
+            .then( () => getDetails())
+            .catch( error => console.log(error));
+    }
+
+    const getDetails = () => {
+        fetch(`${process.env.REACT_APP_API_URL}/api/movies/${mov.id}/`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Token ${token['mr-token']}`
+            }
+        })
+            .then( resp => resp.json())
+            .then( resp => props.updateMovie(resp))
+            .catch( error => console.log(error));
+    }
+
+    return (
+        <React.Fragment>
+            { mov ? (
+                <div>
+                    <h1>{mov.title}</h1>
+                    <p>{mov.description}</p>
+                    <FontAwesomeIcon icon={ faStar } className={mov.avg_ratings > 0 ? 'orange': ''}/>
+                    <FontAwesomeIcon icon={ faStar } className={mov.avg_ratings > 1 ? 'orange': ''}/>
+                    <FontAwesomeIcon icon={ faStar } className={mov.avg_ratings > 2 ? 'orange': ''}/>
+                    <FontAwesomeIcon icon={ faStar } className={mov.avg_ratings > 3 ? 'orange': ''}/>
+                    <FontAwesomeIcon icon={ faStar } className={mov.avg_ratings > 4 ? 'orange': ''}/>
+                    ({mov.no_of_ratings})
+                    <div className="rate-container">
+                        <h2>Rate it</h2>
+                        { [...Array(5)].map((e, i) => {
+                            return <FontAwesomeIcon key={i} icon={ faStar } className={highlighted > i - 1 ? 'purple': ''}
+                                onMouseEnter={highlightedRate(i)}
+                                onMouseLeave={highlightedRate(-1)}
+                                onClick={rateClicked(i)}
+                            />
+                        })
+                        }
+                    </div>
+                </div>
+            ): null}
+        </React.Fragment>
+    )
+}
+
+export default MovieDetails;
